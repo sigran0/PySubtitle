b9580809179a414e7df94b6994eb2f4340642044
unknown <sigran0@gmail.com>
2018-07-24 16:08:53 +0900

    argument parser 완성

--- pysubtitle.py
+++ pysubtitle.py
@@ -1,21 +1,39 @@
 
 import os
 import argparse
+from subtitle.SMI import SMI
+from subtitle.SRT import SRT
+from subtitle.VTT import VTT
 
-def process(file_path, target_path, milsec, convert_to, slice_from, slice_to):
+def process(file_path, target_path, milsec=None, convert_to=None, slice_from=None, slice_to=None, lang='ENCC'):
 
     if not os.path.isfile(file_path):
         raise FileNotFoundError()
 
     if '.smi' in file_path or '.SMI' in file_path:
         file_type = 'smi'
+        subt = SMI()
     elif '.srt' in file_path or '.SRT' in file_path:
         file_type = 'srt'
+        subt = SRT()
     elif '.vtt' in file_path or '.VTT' in file_path:
         file_type = 'vtt'
+        subt = VTT()
     else:
         raise TypeError('this file does not support in this script')
-
+    
+    subt.parse(target_path)
+    
+    if slice_from is not None and slice_to is not None:
+        subt.slice(slice_from, slice_to)
+    
+    if milsec is not None:
+        subt.shift(milsec)
+    
+    if convert_to is not None:
+        subt.convert_to(convert_to)
+    
+    subt.make_file(target_path)
 
 
 if __name__ == '__main__':
@@ -26,5 +44,7 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--convert_to', dest='convert_to', required=False, help='Convert subtitle file')
     parser.add_argument('-z', '--slice_from', dest='slice_from', required=False, help='Slice subtitle file')
     parser.add_argument('-x', '--slice_to', dest='slice_to', required=False, help='Slice subtitle file')
+    parser.add_argument('-l', '--language', dest='lang', required=False, help='Language setting')
 
     args = parser.parse_args()
+    process(args.input_file, args.output_file, args.milsec, args.convert_to, args.slice_from, args.slice_to, args.lang)
